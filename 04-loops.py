478dc23d3818c8a79207017ecf84f816fa5cd89e
Chandra Koushik <infinitekoushik@gmail.com>
2025-07-02 19:42:56 +0530

    Create 04-loops.py

---
+++ 04-loops.py
@@ -0,0 +1,13 @@
+# Loop examples in Python
+
+# FOR loop
+print("For loop from 1 to 5:")
+for i in range(1, 6):
+    print(i)
+
+# WHILE loop
+print("\nWhile loop until 5:")
+x = 1
+while x <= 5:
+    print(x)
+    x += 1
