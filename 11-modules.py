f199259cd95995652a84042fd30af54850062c30
Chandra Koushik <infinitekoushik@gmail.com>
2025-07-02 19:51:39 +0530

    Create 11-modules.py

---
+++ 11-modules.py
@@ -0,0 +1,11 @@
+# Using built-in Python modules
+
+import math
+import random
+
+# Math module
+print("Square root of 25:", math.sqrt(25))
+print("PI value:", math.pi)
+
+# Random module
+print("Random number between 1–100:", random.randint(1, 100))
