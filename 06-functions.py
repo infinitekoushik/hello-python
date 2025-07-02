00e3b27d42b6024feec499e71e5c5c8665c0209f
Chandra Koushik <infinitekoushik@gmail.com>
2025-07-02 19:44:29 +0530

    Create 06-functions.py

---
+++ 06-functions.py
@@ -0,0 +1,11 @@
+# Function example in Python
+
+def greet(name):
+    print("Hello,", name + "!")
+
+def add(a, b):
+    return a + b
+
+greet("Koushik")
+result = add(5, 3)
+print("Sum is:", result)
