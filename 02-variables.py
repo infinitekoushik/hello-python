97a4e55de04c03ef0d0b54a7a2c7d4045ae81239
Chandra Koushik <infinitekoushik@gmail.com>
2025-07-02 19:40:13 +0530

    Create  02-variables.py

---
+++ 02-variables.py
@@ -0,0 +1,7 @@
+# Working with variables and input
+
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+
+print("Hi", name + "!")
+print("You will be", age + 1, "next year.")
